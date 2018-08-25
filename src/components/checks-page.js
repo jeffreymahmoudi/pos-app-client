93ec4c3745c14570dd39e1130b8027720661bb2e
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-08-25 04:17:56 -0500

    Checks page

--- src/components/checks-page.js
+++ src/components/checks-page.js
@@ -1,105 +1,94 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
-import {fetchProtectedData} from '../actions/protected-data';
+import { fetchChecks } from '../actions/checkActions';
 
 import './checks-page.css';
 
 export class ChecksPage extends React.Component {
-    componentDidMount() {
-        this.props.dispatch(fetchProtectedData());
+  componentDidMount = () => {
+    this.props.loadChecksConnect();
+  };
+
+  renderTotal = check => {
+    let total = 0;
+    for (let i = 0; i < check.orderedItems.length; i++) {
+      total += check.orderedItems[i].itemId.price;
+    }
+    return total;
+  };
+
+  renderOrderedItems = check => {
+    const items = check.orderedItems.map((item, index) => (
+      <React.Fragment key={index}>
+        <p>${item.itemId.price}.00 - {item.itemId.name}</p>
+      </React.Fragment>
+    ));
+
+    return items;
+  };
+
+  renderResults = () => {
+    if (this.props.loading) {
+      return <p>Loading checks...</p>;
     }
 
-    render() {
-        return (
-            <div className="checks">
-                <h1>Checks</h1>
-                <p>Order history sorted by last updated.</p>
-                <hr />
-                <div className="v-flex-container">
-                    <div className="check">
-                      <p className="updated"><span className="field">Updated: </span>1/1/1111 24:00:00G</p>
-                      <p className="status"><span className="field">Status: </span>Closed</p>
-                      <p className="table-number"><span className="field">Table: </span>1</p>
-                      <p className="total"><span className="field">Total: </span>$1.00</p>
-                      <details>
-                        <summary>Ordered Items</summary>
-                            <div>
-                                <p>name: namee</p>
-                                <p>price: $100.00</p>
-                            </div>
-                            <div>
-                                <p>name: namee</p>
-                                <p>price: $100.00</p>
-                            </div>
-                            <div>
-                                <p>name: namee</p>
-                                <p>price: $100.00</p>
-                            </div>
-                        </details>
-                    </div>
-                    <div className="check">
-                      <p className="updated"><span className="field">Updated: </span>1/1/1111 24:00:00G</p>
-                      <p className="status"><span className="field">Status: </span>Closed</p>
-                      <p className="table-number"><span className="field">Table: </span>1</p>
-                      <p className="total"><span className="field">Total: </span>$1.00</p>
-                      <details>
-                        <summary>Ordered Items</summary>
-                            <div>
-                                <p>name: namee</p>
-                                <p>price: $100.00</p>
-                            </div>
-                            <div>
-                                <p>name: namee</p>
-                                <p>price: $100.00</p>
-                            </div>
-                            <div>
-                                <p>name: namee</p>
-                                <p>price: $100.00</p>
-                            </div>
-                        </details>
-                    </div>
-                    <div className="check">
-                      <p className="updated"><span className="field">Updated: </span>1/1/1111 24:00:00G</p>
-                      <p className="status"><span className="field">Status: </span>Closed</p>
-                      <p className="table-number"><span className="field">Table: </span>1</p>
-                      <p className="total"><span className="field">Total: </span>$1.00</p>
-                      <details>
-                        <summary>Ordered Items</summary>
-                            <div>
-                                <p>name: namee</p>
-                                <p>price: $100.00</p>
-                            </div>
-                            <div>
-                                <p>name: namee</p>
-                                <p>price: $100.00</p>
-                            </div>
-                            <div>
-                                <p>name: namee</p>
-                                <p>price: $100.00</p>
-                            </div>
-                        </details>
-                    </div>
-                </div>
-                {/* <div className="dashboard-username">
-                    Username: {this.props.username}
-                </div>
-                <div className="dashboard-name">Name: {this.props.name}</div>
-                <div className="dashboard-protected-data">
-                    Protected data: {this.props.protectedData}
-                </div> */}
-            </div>
-        );
+    if (this.props.error) {
+      return <strong>{this.props.error.message}</strong>;
     }
+
+    const checks = this.props.checks.map((check, index) => (
+      // <Link to={`/checks/${check.id}`}>
+      <div className="check-order" key={index}>
+        <p>{new Date(check.updatedAt).toUTCString()}</p>
+        <p>Table: {check.tableId}</p>
+        <p>
+          Total: ${this.renderTotal(check)}
+          .00
+        </p>
+        <details>
+          <summary>Ordered Items</summary>
+          {this.renderOrderedItems(check)}
+        </details>
+      </div>
+      // </Link>
+    ));
+
+    return checks;
+  };
+
+  render = () => {
+    return (
+      <div className="checks-container">
+        <h1>Checks</h1>
+        <p>Order history sorted by last updated.</p>
+        <hr />
+        <div className="v-flex-container">
+          {this.renderResults()}
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    const {currentUser} = state.auth;
-    return {
-        username: state.auth.currentUser.username,
-        name: `${currentUser.firstname} ${currentUser.lastname}`,
-        protectedData: state.protectedData.data
-    };
+  const { currentUser } = state.auth;
+  return {
+    username: state.auth.currentUser.username,
+    name: `${currentUser.firstname} ${currentUser.lastname}`,
+    checks: state.checks.checks,
+    loading: state.checks.loading,
+    error: state.checks.error
+  };
 };
 
-export default requiresLogin()(connect(mapStateToProps)(ChecksPage));
+const mapDispatchToProps = dispatch => ({
+  loadChecksConnect: () => dispatch(fetchChecks())
+});
+
+export default requiresLogin()(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(ChecksPage)
+);
