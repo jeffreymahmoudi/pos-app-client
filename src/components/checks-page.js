3b2453c559f1aa001140477b46fc3b70f91857ff
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-08-24 22:02:26 -0500

    Create checks page

--- src/components/checks-page.js
+++ src/components/checks-page.js
@@ -3,6 +3,8 @@ import {connect} from 'react-redux';
 import requiresLogin from './requires-login';
 import {fetchProtectedData} from '../actions/protected-data';
 
+import './checks-page.css';
+
 export class ChecksPage extends React.Component {
     componentDidMount() {
         this.props.dispatch(fetchProtectedData());
@@ -10,15 +12,79 @@ export class ChecksPage extends React.Component {
 
     render() {
         return (
-            <div className="tables">
+            <div className="checks">
                 <h1>Checks</h1>
-                <div className="dashboard-username">
+                <p>Order history sorted by last updated.</p>
+                <hr />
+                <div className="v-flex-container">
+                    <div className="check">
+                      <p className="updated">Updated At</p>
+                      <p className="table-number">Table: 1</p>
+                      <p className="total">Total: $1.00</p>
+                      <details>
+                        <summary>Ordered Items</summary>
+                            <div>
+                                <p>name: namee</p>
+                                <p>price: $100.00</p>
+                            </div>
+                            <div>
+                                <p>name: namee</p>
+                                <p>price: $100.00</p>
+                            </div>
+                            <div>
+                                <p>name: namee</p>
+                                <p>price: $100.00</p>
+                            </div>
+                        </details>
+                    </div>
+                    <div className="check">
+                      <p className="updated">Updated At</p>
+                      <p className="table-number">Table: 1</p>
+                      <p className="total">Total: $1.00</p>
+                      <details>
+                        <summary>Ordered Items</summary>
+                            <div>
+                                <p>name: namee</p>
+                                <p>price: $100.00</p>
+                            </div>
+                            <div>
+                                <p>name: namee</p>
+                                <p>price: $100.00</p>
+                            </div>
+                            <div>
+                                <p>name: namee</p>
+                                <p>price: $100.00</p>
+                            </div>
+                        </details>
+                    </div>
+                    <div className="check">
+                      <p className="updated">Updated At</p>
+                      <p className="table-number">Table: 1</p>
+                      <p className="total">Total: $1.00</p>
+                      <details>
+                        <summary>Ordered Items</summary>
+                            <div>
+                                <p>name: namee</p>
+                                <p>price: $100.00</p>
+                            </div>
+                            <div>
+                                <p>name: namee</p>
+                                <p>price: $100.00</p>
+                            </div>
+                            <div>
+                                <p>name: namee</p>
+                                <p>price: $100.00</p>
+                            </div>
+                        </details>
+                    </div>
+                </div>
+                {/* <div className="dashboard-username">
                     Username: {this.props.username}
                 </div>
                 <div className="dashboard-name">Name: {this.props.name}</div>
                 <div className="dashboard-protected-data">
                     Protected data: {this.props.protectedData}
-                </div>
+                </div> */}
             </div>
         );
     }
